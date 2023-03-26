c9ac41a64a8ab717131dde3b34c46e5bbc9cbf4b
Itxaso Aizpurua <itsaso23a@gmail.com>
2023-03-26 16:30:15 +0200

    run black, flake8 and isort

--- clustintime/cli/run_clustintime.py
+++ clustintime/cli/run_clustintime.py
@@ -43,7 +43,8 @@ def _get_parser():
         "--component",
         dest="component",
         type=str,
-        help="Desired component of the signal to analyze, the options are `whole`," "`positive`, `negative`.",
+        help="Desired component of the signal to analyze, the options are `whole`,"
+        "`positive`, `negative`.",
         required=False,
         default="whole",
     )
@@ -63,7 +64,8 @@ def _get_parser():
         "--correlation",
         dest="correlation",
         type=str,
-        help="Desired type of correlation, the options are `standard`, `window`" "Default is `standard`",
+        help="Desired type of correlation, the options are `standard`, `window`"
+        "Default is `standard`",
         required=False,
         default="standard",
     )
@@ -105,7 +107,8 @@ def _get_parser():
         "--threshold",
         dest="thr",
         type=int,
-        help="If '-p' is used and takes the value 'thr', " "It will be used as a threshold percentile",
+        help="If '-p' is used and takes the value 'thr', "
+        "It will be used as a threshold percentile",
         required=False,
         default=95,
     )
@@ -155,7 +158,8 @@ def _get_parser():
         "--linkage",
         dest="linkage",
         type=str,
-        help="Linkage criterion for the 'Agglomerative Clustering' algorithm. To see the available options see documentation of each algorithm"
+        help="Linkage criterion for the 'Agglomerative Clustering' algorithm."
+        "To see the available options see documentation of each algorithm"
         "Default is `ward`",
         required=False,
         default="ward",
@@ -194,7 +198,8 @@ def _get_parser():
         "--saving-dir",
         dest="saving_dir",
         type=str,
-        help="The name or fullpath to saving directory." "Default is to save in the current directory",
+        help="The name or fullpath to saving directory."
+        "Default is to save in the current directory",
         required=False,
         default=".",
     )
