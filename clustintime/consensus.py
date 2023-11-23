226cd6b91606c5bc7ed6e2c90f3b3ce7fe0da852
Cristina-Tobias <cristina_tobias@hotmail.es>
2023-11-23 15:38:09 +0100

    corrected the imports

--- clustintime/consensus.py
+++ clustintime/consensus.py
@@ -3,7 +3,9 @@ import random
 import numpy as np
 import pandas as pd
 
-from clustintime.clustintime.clustintime.clustering import Clustering
+from clustintime.clustering import Clustering
+
+
 
 def compute_connectivity_matrix(n_items, labels):
     connectivity_matrix = np.zeros([n_items, n_items])
@@ -55,12 +57,14 @@ def bfs(graph, source, dest):
 
 
 class Consensus:
-    def __init__(self, algorithm):
+    def __init__(self, clustering_parameters, algorithm, algorithm_parameters):
+        self.clustering_parameters = clustering_parameters
         self.algorithm = algorithm
+        self.algorithm_parameters = algorithm_parameters
 
 
     def find_clusters_with_consensus(self):
-        npoints = len(self.algorithm.indices)
+        npoints = len(self.clustering_parameters.indices)
         sum_connectivity_matrix = np.zeros([npoints, npoints])
         index_matrix_sum = np.zeros([npoints, npoints])
 
@@ -71,10 +75,9 @@ class Consensus:
                 index_matrix = pd.DataFrame([0] * npoints)
                 index_matrix[0][sampling] = 1
                 index_matrix_sum = index_matrix_sum + np.dot(index_matrix, np.transpose(index_matrix))
-                data_sampled = self.algorithm.corr_map[sampling, :][:, sampling]
-
-                idx = self.get_indices(data_sampled, self.algorithm.indices)
-
+                data_sampled = self.clustering_parameters.corr_map[sampling, :][:, sampling]
+                idx = self.apply_algorithm(data_sampled)
+                idx = idx[~np.isnan(idx)]
                 idx = np.transpose(pd.DataFrame([idx, sampling]))
                 idx = idx.set_index(1)
                 idx = idx[np.logical_not(np.isnan(idx[0]))]
@@ -84,38 +87,47 @@ class Consensus:
             sum_connectivity_matrix = sum_connectivity_matrix + connectivity_matrix
             _consensus = np.divide(sum_connectivity_matrix, index_matrix_sum)
             _consensus[_consensus < find_threshold_bfs(_consensus)] = 0
-
-            final_labels = self.get_labels(_consensus, self.algorithm.indices)
-
+            final_labels = self.apply_algorithm(_consensus)
             thr = find_threshold_bfs(_consensus)
             _consensus[_consensus <= thr] = 0
-            if self.algorithm in (Clustering.info_map, Clustering.greedy_mod, Clustering.louvain):
-                final_labels = final_labels[1]
+            
             whole_connectivity_matrix = compute_connectivity_matrix(npoints, final_labels)
 
             are_clusters_stable = self.check_if_clusters_stable(
-                _consensus, self.algorithm.indices, npoints, whole_connectivity_matrix
+                _consensus, npoints, whole_connectivity_matrix
             )
 
         return final_labels
+    def apply_algorithm(self, data):
+        if self.algorithm == "infomap":
+            corr_map, labels = Clustering(data,
+                                          self.clustering_parameters.indices, 
+                                          self.clustering_parameters.nscans ).info_map(self.algorithm_parameters)
+        elif self.algorithm == "KMeans":
+            labels = Clustering(data,
+                                self.clustering_parameters.indices,
+                                self.clustering_parameters.nscans ).k_means(
+                *self.algorithm_parameters
+            )
+        elif self.algorithm == "Agglomerative":
+            labels = Clustering(data,
+                                self.clustering_parameters.indices,
+                                self.clustering_parameters.nscans ).agglomerative_clustering( 
+                *self.algorithm_parameters)
+        elif self.algorithm == "Louvain":
+            corr_map, labels = Clustering(data,
+                                          self.clustering_parameters.indices, 
+                                          self.clustering_parameters.nscans ).louvain(self.algorithm_parameters)
+        elif self.algorithm == "Greedy":
+            corr_map, labels = Clustering(data,
+                                          self.clustering_parameters.indices, 
+                                          self.clustering_parameters.nscans ).greedy_mod(self.algorithm_parameters)
 
-    def get_indices(self, data_sampled, indices):
-        indices = self.get_labels(data_sampled, indices)
-        if self.algorithm in (Clustering.info_map, Clustering.greedy_mod, Clustering.louvain):
-            return indices[1]
-        return indices
+        return labels
 
-    def get_labels(self, data_sampled, indices):
-        if self.algorithm in (Clustering.info_map, Clustering.greedy_mod, Clustering.louvain):  # pylint: disable=comparison-with-callable
-            return self.algorithm(data_sampled, indices, self.threshold, self.n_scans)
-        else:
-            return self.algorithm(data_sampled, indices, self.n_scans, self.n_clusters)
-
-    def check_if_clusters_stable(self, _consensus, indices, npoints, whole_connectivity_matrix):
+    def check_if_clusters_stable(self, _consensus, npoints, whole_connectivity_matrix):
         for _ in range(100):
-            labels = self.get_labels(_consensus, indices)
-            if self.algorithm in (Clustering.info_map, Clustering.greedy_mod, Clustering.louvain):
-                labels = labels[1]
+            labels = self.apply_algorithm(_consensus)
             connect = compute_connectivity_matrix(npoints, labels)
 
             if not np.array_equal(whole_connectivity_matrix, connect):
