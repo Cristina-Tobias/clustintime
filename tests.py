2674ce8ba978db8f11fc2331dd141c2934cb1744
Zalteck <ferztk@gmail.com>
2023-11-23 15:41:47 +0100

    cleaning first test

--- tests.py
+++ tests.py
@@ -2,8 +2,8 @@ from clustintime.pipeline import load_data
 import numpy as np
 
 def test_load_data():
-    data_file = "C://Users//ferzt//Documents//Brainhack//clustintime//test//data//100206_stabsel_spike_MvMEPFM_G50.THR_95.STC.DR2.nii.gz"
-    mask_file = "C://Users//ferzt//Documents//Brainhack//clustintime//test//data//mask.nii.gz"
+    data_file = ".//data//100206_stabsel_spike_MvMEPFM_G50.THR_95.STC.DR2.nii.gz"
+    mask_file = ".//data//mask.nii.gz"
     data_masked, masker, nscans = load_data(data_paths=data_file, mask_paths=mask_file)
 
 
@@ -11,9 +11,3 @@ def test_load_data():
     assert str(type(masker)) == "<class 'nilearn.maskers.nifti_masker.NiftiMasker'>"
     assert nscans == [284]
 
-def test_stupid():
-    prueba = np.ones([3,3])
-    assert prueba.shape == (3,3)
-    
-# print('Hello')
-test_stupid()
