2b9fb52906ca087b502c404cb4d4338932fc1356
Zalteck <ferztk@gmail.com>
2023-11-23 14:56:26 +0100

    first test is working

---
+++ tests.py
@@ -0,0 +1,17 @@
+from clustintime.pipeline import load_data
+import numpy as np
+
+def test_load_data():
+    data_file = "C://Users//ferzt//Documents//Brainhack//clustintime//test//data//100206_stabsel_spike_MvMEPFM_G50.THR_95.STC.DR2.nii.gz"
+    mask_file = "C://Users//ferzt//Documents//Brainhack//clustintime//test//data//mask.nii.gz"
+    data_masked, masker, nscans = load_data(data_paths=data_file, mask_paths=mask_file)
+
+
+    assert data_masked.shape == (284,1936)
+
+def test_stupid():
+    prueba = np.ones([3,3])
+    assert prueba.shape == (3,3)
+    
+# print('Hello')
+test_stupid()
